280544d6f53e7a0da3a226410c8aceb5a1a056d0
Wonjoon <junwonjoon41@gmail.com>
2024-04-20 19:54:21 -0700

    Fixed formatting again

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 """
-    Easy Azure Streamlit Demo
     Original Author: Wolf Paulus (wolf@paulus.com)
     Forked by Wonjoon Jun
     This is streamlit code that converts url to csv file.
@@ -67,10 +66,10 @@ def ui() -> int:
     try:
         list_of_dfs = get_data_from_url(url)
         length_dfs = len(list_of_dfs)
-        st.write(f"Total {length_dfs} table{"
-        s
-        " if length_dfs > 1 else " "}found on the webpage")
-        except BaseException:
+        st.write(
+            f"Total {length_dfs} table{
+                "s" if length_dfs > 1 else ""} found on the webpage")
+    except BaseException:
         st.error(f"Could not find Table in the URL: {url}")
         return 0
     i = 1
