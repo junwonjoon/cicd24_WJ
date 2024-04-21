52a71a25d846665b73fa53d67d2debcf917274b2
Wonjoon <junwonjoon41@gmail.com>
2024-04-20 18:44:58 -0700

    changed formatting

--- src/app.py
+++ src/app.py
@@ -11,6 +11,7 @@ import pandas as pd
 import numpy as np
 import requests
 
+
 def is_website_up(url):
     """
     Checks validity of the URL, this function is only used for pytest.
@@ -24,7 +25,7 @@ def is_website_up(url):
     - requests.exceptions.RequestException: If an error occurs during the HTTP request
     """
     try:
-        response = requests.get(url, timeout=10)  
+        response = requests.get(url, timeout=10)
         if response.status_code < 400:
             return True
         else:
@@ -32,9 +33,10 @@ def is_website_up(url):
     except requests.exceptions.RequestException as e:
         print(f"Error checking website: {e}")
         return False
-    
+
+
 @st.cache_resource()
-def get_data_from_url(url:str) -> pd.DataFrame:
+def get_data_from_url(url: str) -> pd.DataFrame:
     """
     This is function to convert the URL's table into pd.DataFrame to be later processed.
     This is made into seperate function, in order to cache resources.
@@ -46,6 +48,7 @@ def get_data_from_url(url:str) -> pd.DataFrame:
     """
     return pd.read_html(url)
 
+
 def ui() -> int:
     """
     This the mainpage's UI
@@ -53,32 +56,35 @@ def ui() -> int:
     - int: number of tables created.
     """
     st.set_page_config(
-    page_title="Wonjoon's CSV Generator",
-    page_icon="📃"
+        page_title="Wonjoon's CSV Generator",
+        page_icon="📃"
     )
     st.title("URL to CSV Converter")
     st.subheader("Enter a wikipedia page, or any URL with HTML table")
-    url = st.text_input("Enter URL here:","https://en.wikipedia.org/wiki/List_of_countries_by_real_GDP_growth_rate")
+    url = st.text_input(
+        "Enter URL here:",
+        "https://en.wikipedia.org/wiki/List_of_countries_by_real_GDP_growth_rate")
     try:
         list_of_dfs = get_data_from_url(url)
         st.write(f"Total {len(list_of_dfs)} table(s) found on the webpage")
-    except:
+    except BaseException:
         st.error(f"Could not find Table in the URL: {url}")
         return 0
     i = 1
     for data in list_of_dfs:
         st.header(f"Table {i}")
-        #st.table can still fail with try. So, try was removed here.
+        # st.table can still fail with try. So, try was removed here.
         st.table(data)
         csv = data.to_csv().encode('utf-8')
         st.download_button(
-        label=f"Download Table {i} as CSV",
-        data=csv,
-        file_name=f'Table_{i}.csv',
-        mime='text/csv',
+            label=f"Download Table {i} as CSV",
+            data=csv,
+            file_name=f'Table_{i}.csv',
+            mime='text/csv',
         )
         i += 1
     return len(list_of_dfs)
+
 
 if __name__ == "__main__":
     data_converted = ui()
